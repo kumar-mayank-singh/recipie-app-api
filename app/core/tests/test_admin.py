47dfe60fe5b98e37f81df0bbaa18f45627591e38
mayank <007.singh.mayank@gmail.com>
2021-01-12 16:07:25 +0530

    Updated admin to support admin user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -15,5 +15,31 @@ class AdminSiteTests(TestCase):
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='test@dev.com',
-            password='password12@'
+            password='password12@',
+            name='Test user Full name'
         )
+
+    def test_user_listed(self):
+        """Test user as listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_page_change(self):
+        """Test that user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        #admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Tests that create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+
+
